0cdde5036f7495dee326683c9588fb6b2ebca9ed
eosun77 <eosun77>
2023-07-06 14:03:39 +0900

    [Feat] Header 컴포넌트 제작

--- frontend/pages/create.tsx
+++ frontend/pages/create.tsx
@@ -3,7 +3,7 @@ import TextInput from "@/components/TextInput";
 
 function create() {
   return (
-    <div className="flex flex-col gap-4 px-16">
+    <div className="flex flex-col gap-4 px-16 pt-4">
       <TextInput placeholder="게임의 제목을 입력해주세요" />
       <div className="flex gap-36">
         <Candidate color="red" />

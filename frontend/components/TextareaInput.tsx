c981882e19fdbb52474d0895afee5914c9020756
eosun77 <eosun77>
2023-07-06 13:45:35 +0900

    [Feat] 생성 페이지 제작

--- frontend/components/TextareaInput.tsx
+++ frontend/components/TextareaInput.tsx
@@ -1,19 +1,14 @@
-import { BORDER_COLORS } from "@/constants/color";
-import { getBorderColor } from "@/utils/getBorderColor";
-
 interface Props {
   placeholder?: string;
   color?: "red" | "blue";
   maxLength?: number;
-  height: string;
+  height?: string;
 }
 
 function TextareaInput({ placeholder, color, maxLength, height }: Props) {
   return (
     <textarea
-      className={`border-2 rounded-lg p-2 w-full resize-none ${getBorderColor(
-        color
-      )} ${height}`}
+      className={`border-2 rounded-lg p-2 w-full resize-none ${height} ${color}`}
       placeholder={placeholder}
       maxLength={maxLength}
     />

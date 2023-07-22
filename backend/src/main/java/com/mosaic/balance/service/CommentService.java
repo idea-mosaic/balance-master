66660bfa0302de25f082a78238626517ac0c6a43
JODONG2 <whgusehd96@gmail.com>
2023-07-22 21:32:54 +0900

    [Docs] CommentService add annotation

--- backend/src/main/java/com/mosaic/balance/service/CommentService.java
+++ backend/src/main/java/com/mosaic/balance/service/CommentService.java
@@ -3,8 +3,41 @@ package com.mosaic.balance.service;
 import com.mosaic.balance.dto.CommentDTO;
 
 public interface CommentService {
+    /**
+     * Create Comment
+     * @param gameSeq
+     * @param requestCreateDTO
+     * @return PK of created comment and created date
+     */
     CommentDTO.ResponseCreateDTO createComment(long gameSeq, CommentDTO.RequestCreateDTO requestCreateDTO);
+
+    /**
+     * Get Comment details
+     * @param gameSeq
+     * @param requestReadDTO
+     * @return detail comments each color
+     * @throws com.mosaic.balance.exception.CommentException GAME_NOT_EXIST
+     */
     CommentDTO.ResponseReadDTO getComments(long gameSeq, CommentDTO.RequestReadDTO requestReadDTO);
+
+    /**
+     * Modify comment
+     * @param gameSeq
+     * @param commentSeq
+     * @param requestUpdateDTO
+     * @return PK of created comment and modified date
+     * @throws com.mosaic.balance.exception.CommentException PWD_INCORRECT
+     * @throws com.mosaic.balance.exception.CommentException GAME_NOT_EXIST
+     * @throws com.mosaic.balance.exception.CommentException COMMENT_NOT_EXIST
+     */
     CommentDTO.ResponseUpdateDTO updateComment(long gameSeq, long commentSeq, CommentDTO.RequestUpdateDTO requestUpdateDTO);
+
+    /**
+     * Delete Comment
+     * @param commentSeq
+     * @param requestDeleteDTO
+     * @throws com.mosaic.balance.exception.CommentException PWD_INCORRECT
+     * @throws com.mosaic.balance.exception.CommentException GAME_NOT_EXIST
+     */
     void deleteComment(long commentSeq, CommentDTO.RequestDeleteDTO requestDeleteDTO);
 }
